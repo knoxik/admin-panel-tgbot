3003c101e57bbe1318b9b5bfe80b3c18a3816e34
Kirill <knoxxrus2@gmail.com>
2022-04-26 17:55:35 +0500

    feat: сверстать секцию постов

--- scripts/script.js
+++ scripts/script.js
@@ -1,12 +1,19 @@
 const toggleButton = document.querySelector('.header__btn-toggle');
-const page = document.querySelector('.page')
+const page = document.querySelector('.page');
+const newPostMenu = document.querySelector('.new-post-menu');
+const newPostMenuButton = document.querySelector('.posts__btn')
 
 function changeTheme() {
   page.classList.toggle('page_theme_light');
 }
 
+function togglePostMenu() {
+  newPostMenu.classList.toggle('new-post-menu_active')
+}
+
 function main() {
   toggleButton.addEventListener('click', changeTheme);
+  newPostMenuButton.addEventListener('click', togglePostMenu)
 }
 
 main();
